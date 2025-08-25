5b0cff271251e9fe9a141ca75b3a1c366a970dd0
Vladislav <neivo@vladislav.dev>
2025-08-25 17:32:49 +0500

    update url sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mirchan.vercel.app',
+      url: 'https://mirchan.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
