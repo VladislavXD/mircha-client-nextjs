1154c37fcd02d00cb0c8abd93c8d9eaaa2153d3b
Vladislav <neivo@vladislav.dev>
2025-08-31 00:28:51 +0500

    update serverl api link

--- src/constants/api.url.ts
+++ src/constants/api.url.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://mirchan-expres-api.onrender.com' : 'http://localhost:5000'
+export const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://mirchan-expres-api.vercel.app' : 'http://localhost:5000'
 // NODE_ENV это системная переменная которая сама определяется при деплои проекта
 
 
-// for deploy     https://netify-epress-api.onrender.com
+// for deploy     https://mirchan-expres-api.vercel.app
