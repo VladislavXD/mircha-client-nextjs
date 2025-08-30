5fafa464b79bc1b23f26182c1008451e04346052
Vladislav <neivo@vladislav.dev>
2025-08-31 01:02:19 +0500

    update env link for hheroku

--- src/constants/api.url.ts
+++ src/constants/api.url.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://mirchan-expres-api.vercel.app' : 'http://localhost:5000'
+export const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://mirchan-expres-api-05347abf4b22.herokuapp.com' : 'http://localhost:5000'
 // NODE_ENV это системная переменная которая сама определяется при деплои проекта
 
 
-// for deploy     https://mirchan-expres-api.vercel.app
+// for deploy     https://mirchan-expres-api-05347abf4b22.herokuapp.com/
