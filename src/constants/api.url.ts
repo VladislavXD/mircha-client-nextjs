c553f78980a073de585b46d4a5522fc5b0a0cc0f
Vladislav <neivo@vladislav.dev>
2025-09-01 14:45:40 +0500

    update api url

--- src/constants/api.url.ts
+++ src/constants/api.url.ts
@@ -1,5 +1,8 @@
-export const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://mirchan-expres-api-05347abf4b22.herokuapp.com' : 'http://localhost:5000'
+export const BASE_URL = process.env.NODE_ENV == 'production' ? 'https://mirchan-expres-api.onrender.com' : 'http://localhost:5000'
 // NODE_ENV это системная переменная которая сама определяется при деплои проекта
 
 
-// for deploy     https://mirchan-expres-api-05347abf4b22.herokuapp.com/
+// for deploy heroku     https://mirchan-expres-api-05347abf4b22.herokuapp.com/
+// for deploy render     https://mirchan-expres-api.onrender.com
+
+
